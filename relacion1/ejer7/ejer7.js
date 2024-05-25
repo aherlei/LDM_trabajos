fede0b1e905b43c71acedf25315f564a18958d42
aherlei <aherlei564@g.educaand.es>
2024-05-25 20:42:21 +0200

    añadiendo modo oscuro

--- relacion1/ejer7/ejer7.js
+++ relacion1/ejer7/ejer7.js
@@ -1,5 +1,7 @@
 const btn   = document.getElementById("btn")
 btn.addEventListener("click", diviEuclides)
+// const btnDark = document.addEventListener("click", modoOscuro)
+// const btnLight = document.addEventListener("click", modoClaro)
 
 function diviEuclides() {
     
@@ -22,11 +24,22 @@ function diviEuclides() {
             }
             document.getElementById("resultado").innerHTML = `el cociente es : ${cociente} y el resto: ${resto} `;
         }
-
     } else {
         resultado.innerHTML = "<span class='text-danger'> Alguno de los campos esta vacio </span>"
     }
-    
-    
   
 }
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+darkButton.addEventListener('click', function() {
+    body.classList.add('dark-mode');
+    body.classList.remove('light-mode');
+});
+
+lightButton.addEventListener('click', function() {
+    body.classList.add('light-mode');
+    body.classList.remove('dark-mode');
+});
